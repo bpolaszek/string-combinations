e5d44bbb7409f67c217944923c80ba698b556f99
Beno!t POLASZEK <bpolaszek@gmail.com>
2022-11-11 07:10:36 +0100

    chore: cleanup

--- src/functions.php
+++ src/functions.php
@@ -5,36 +5,10 @@ namespace BenTools\StringCombinations;
 require_once __DIR__ . '/StringCombinations.php';
 
 /**
- * @param $charset
- * @param int      $min
- * @param int|null $max
- * @param string   $glue
+ * @param string|array $charset
  * @return StringCombinations
  */
-function string_combinations($charset, $min = 1, $max = null, $glue = '')
+function string_combinations($charset, int $min = 1, ?int $max = null, string $glue = ''): StringCombinations
 {
     return new StringCombinations($charset, $min, $max, $glue);
-}
-
-/**
- * @param $number
- * @return float|int
- */
-function fact($number)
-{
-    if (extension_loaded('gmp')) {
-        return gmp_intval(gmp_fact($number));
-    }
-    for ($x = $number, $factorial = 1; $x >= 1; $x--) {
-        $factorial = $factorial * $x;
-    }
-    return $factorial;
-}
-
-
-function random_int($min, $max)
-{
-    static $callable;
-    $callable = function_exists('random_int') ? 'random_int' : 'mt_rand';
-    return $callable($min, $max);
 }
